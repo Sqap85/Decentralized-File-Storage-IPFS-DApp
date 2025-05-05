3c8abcc78cb7527b29bf6a80314530cadb07f03f
Engin Elibol <148553397+Sqap85@users.noreply.github.com>
2025-05-05 21:55:30 +0300

    85

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-//// filepath: /Users/enginelibol/Desktop/Decentralized File Storage (IPFS) DApp/ipfs-dapp/frontend/src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
